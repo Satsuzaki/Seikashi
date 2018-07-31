b2ad2990b1fbc5979cafe525ecaeaaa1f403e28a
Satsuzaki <41466960+Satsuzaki@users.noreply.github.com>
2018-07-31 20:03:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,16 +15,17 @@ bot.on("message", function(message) {
     if (!message.content.startsWith(prefix)) return;
     var args = message.content.substring(prefix.length).split(" ");
     switch (args[0].toLowerCase()) {
-        /*case "roll":
-        let message = msg.content.split(" ");
-		let maxval = parseInt(message[1]);
-		if ((maxval > 10000) || (maxval < 1)) {
-			return msg.reply('You cannot roll more than 10000 or less than 1!')
-		}
-		let min = Math.ceil(0);
-		let max = Math.floor(maxval);
-		return msg.channel.send(Math.floor(Math.random() * (max - min) + min));
-        break;*/
+        case "coin":
+        var replys = [
+            ":black_circle: Pile",
+            ":red_circle: Face"
+        ];
+        let reponse = (replys[Math.floor(Math.random() * replys.length)])
+        var bembed = new Discord.RichEmbed()
+        .setDescription("**__:black_circle: Pile ou Face :red_circle:__**")
+        .addField("_Réponse:__", reponse)
+        message.channel.sendEmbed(bembed)
+	break;
         case "8ball":
         let args = message.content.split(" ").slice(1);
         let tte = args.join(" ")
@@ -39,7 +40,7 @@ bot.on("message", function(message) {
         ];
         let reponse = (replys[Math.floor(Math.random() * replys.length)])
         var bembed = new Discord.RichEmbed()
-        .setDescription("**__:8ball: 8ball__**")
+        .setDescription("**__:8ball: 8ball :8ball:__**")
         .addField("__Question:__", tte)
         .addField("__Réponse:__", reponse)
         message.channel.sendEmbed(bembed)
